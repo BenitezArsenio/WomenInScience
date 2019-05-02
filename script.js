0a459ca7993c561fd705de84514a4c290f9ce173
Arsenio Benitez Jr <Ar15macbook@Arsenios-MacBook-Pro.local>
2019-05-02 11:06:29 -0400

    jj

--- script.js
+++ script.js
@@ -29,13 +29,13 @@ Promise.all([mapP,abbrP])
 
   console.log(geoData,states);
 
-  drawMap(geoData);
+  drawMap(geoData,states);
 
 
 
 });
 
-var drawMap = function(geoData)
+var drawMap = function(geoData,stateArray)
 {
   var screen = {width:1500,height:1000}
     //create Projection
@@ -48,6 +48,12 @@ var drawMap = function(geoData)
     var svg = d3.select("svg")
                 .attr("width",screen.width)
                 .attr("height",screen.height);
+// split states by region
+    var nE = ['MD','DE','NJ','PA','NY',"CT",'RI','MA','NH','VT','ME']
+    var south = ['KY','WV','VA','TN','NC','SC','GA','FL','AL','MS','LA','TX','OK','AR']
+    var mW = ['OH','IN','MI','IL','WI','MO','IA','MN','KS','NE','SD','ND']
+    var west = ['OR','WA','CA','NV','ID','MT','WY','UT','CO','AZ','NM']
+
 
     var states = svg.append("g")
       .selectAll("g")
@@ -58,10 +64,67 @@ var drawMap = function(geoData)
       states.append("path")
       .attr("d",geoGenerator)
       .attr("stroke","black")
-      .attr("fill","none");
+      .attr("fill",function(d,i)
+      {
+        //console.log(s,d.properties.ABBR)
+        if(nE.includes(d.properties.ABBR ))
+        {
+          return 'blue';
+        }
+        else if (south.includes(d.properties.ABBR ))
+        {
+          return 'yellow';
+        }
+        else if (mW.includes(d.properties.ABBR ))
+        {
+          return 'green';
+        }
+        else if (west.includes(d.properties.ABBR ))
+        {
+          return 'orange';
+        }
+      });
 
-     states.append("text")
-      .attr("x",function(d) {return geoGenerator.centroid(d)[0]})
-      .attr("y",function(d) {return geoGenerator.centroid(d)[1]})
-      .text(function(d){return d.properties.ABBR});
+     // states.append("text")
+     //  .attr("x",function(d) {return geoGenerator.centroid(d)[0]})
+     //  .attr("y",function(d) {return geoGenerator.centroid(d)[1]})
+     //  .text(function(d){return d.properties.ABBR});
+      states.append('circle')
+
+            .attr('cx',function(d)
+            {
+              if(d.properties.ABBR == 'MA'||d.properties.ABBR == 'CA'||d.properties.ABBR == 'WI'||d.properties.ABBR == 'NC'  )
+              {
+                return (geoGenerator.centroid(d)[0])
+              }
+              else
+              {
+                return (0)
+              }
+            })
+            .attr('cy',function(d)
+            {
+              if(d.properties.ABBR == 'MA'||d.properties.ABBR == 'CA'||d.properties.ABBR == 'WI'||d.properties.ABBR == 'NC')
+              {
+                return (geoGenerator.centroid(d)[1])
+              }
+              else
+              {
+                return (0)
+              }
+            })
+            .attr('r',7)
+            .attr('fill',function(d)
+            {
+              if(d.properties.ABBR == 'MA'||d.properties.ABBR == 'CA'||d.properties.ABBR == 'WI'||d.properties.ABBR == 'NC' )
+              {
+                console.log(d.properties.ABBR)
+                return 'black'
+              }
+              else
+              {
+                return 'none'
+              }
+
+            })
 };
